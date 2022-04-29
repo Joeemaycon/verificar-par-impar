9070bde322646b8fbaabcd90453331ebcda54207
Joe Maycon <joeemaycon@gmail.com>
2022-04-29 02:34:44 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,14 @@
+# While para que o Programa não feche
+while True:
+
+    try:
+        # Input para pedir o valor
+        valor = int(input('Digite um valor: '))
+        # Comparando o Valor
+        if valor % 2 == 0:
+            print('Par')
+        else:
+            print('Impar')
+    except:
+        # Caso o usuario digite uma string
+        print('Digite somente números')
